1f6c2605f9641b2f414406fa7d5b291f58ca1d56
breadbyte <mkjego@gmail.com>
2019-09-20 02:36:27 +0800

    Initial - Add Avalonia UI & gitignore.

---
+++ src/FreeLauncherAvaloniaUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeLauncherAvaloniaUI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
---
+++ src/FreeLauncherAvaloniaUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace FreeLauncherAvaloniaUI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject { }
+}
